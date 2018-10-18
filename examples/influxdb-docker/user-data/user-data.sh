4114c14fb10df98f1d5a46886cb0024aae6c1d5d
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-10-18 15:04:15 +0100

    update docker example to include mounting volumes

--- examples/influxdb-docker/user-data/user-data.sh
+++ examples/influxdb-docker/user-data/user-data.sh
@@ -6,12 +6,33 @@ set -e
 # From: https://alestic.com/2010/12/ec2-user-data-output/
 exec > >(tee /user-data/mock-user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
+source "/opt/influxdb-commons/mount-volume.sh"
+
+function mount_volumes {
+  local -r volume_device_name="$1"
+  local -r volume_mount_point="$2"
+  local -r volume_owner="$3"
+
+  echo "Mounting EBS Volume for meta, data, wal and hh directories"
+  mount_volume "$volume_device_name" "$volume_mount_point" "$volume_owner"
+}
+
 function run {
   local -r asg_name="$1"
   local -r aws_region="$2"
   local -r license_key="$3"
   local -r shared_secret="$4"
+  local -r volume_device_name="$5"
+  local -r volume_mount_point="$6"
+  local -r volume_owner="$7"
   local -r hostname="$(hostname)"
+
+  mount_volumes "$volume_device_name" "$volume_mount_point" "$volume_owner"
+
+  local -r meta_dir="$volume_mount_point/var/lib/influxdb/meta"
+  local -r data_dir="$volume_mount_point/var/lib/influxdb/data"
+  local -r wal_dir="$volume_mount_point/var/lib/influxdb/wal"
+  local -r hh_dir="$volume_mount_point/var/lib/influxdb/hh"
 
   "/opt/influxdb/bin/run-influxdb" \
     --node-type "meta" \
@@ -19,7 +40,13 @@ function run {
     --data-asg-name "$asg_name" \
     --region "$aws_region" \
     --auto-fill "<__HOST_NAME__>=$hostname" \
-    --auto-fill "<__LICENSE_KEY__>=$license_key"
+    --auto-fill "<__LICENSE_KEY__>=$license_key" \
+    --auto-fill "<__META_DIR__>=$meta_dir"
+
+  # The services are configured to restart on error (which it will because the config files are in an invalid state at instance boot)
+  # we risk hitting the "Start request repeated too quickly" SystemD error. So we wait a few seconds before we run the data service
+  # See: https://serverfault.com/questions/845471/service-start-request-repeated-too-quickly-refusing-to-start-limit/845473#845473
+  sleep 5
 
   "/opt/influxdb/bin/run-influxdb" \
     --node-type "data" \
@@ -28,11 +55,18 @@ function run {
     --region "$aws_region" \
     --auto-fill "<__HOST_NAME__>=$hostname" \
     --auto-fill "<__LICENSE_KEY__>=$license_key" \
-    --auto-fill "<__SHARED_SECRET__>=$shared_secret"
+    --auto-fill "<__SHARED_SECRET__>=$shared_secret" \
+    --auto-fill "<__META_DIR__>=$meta_dir" \
+    --auto-fill "<__DATA_DIR__>=$data_dir" \
+    --auto-fill "<__WAL_DIR__>=$wal_dir" \
+    --auto-fill "<__HH_DIR__>=$hh_dir"
 }
 
 run \
   "${cluster_asg_name}" \
   "${mock_aws_region}" \
   "${license_key}" \
-  "${shared_secret}"
+  "${shared_secret}" \
+  "${volume_device_name}" \
+  "${volume_mount_point}" \
+  "${volume_owner}"
