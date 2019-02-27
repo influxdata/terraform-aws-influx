5459c2be29e31644a4cea024e4b7d3594e9cc579
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2019-02-27 16:44:41 +0100

    [skip ci] update count expression

--- modules/chronograf-security-group-rules/main.tf
+++ modules/chronograf-security-group-rules/main.tf
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_security_group_rule" "http_port_cidr_blocks" {
-  count             = "${signum(length(var.http_port_cidr_blocks))}"
+  count             = "${length(var.http_port_cidr_blocks) >= 1 ? 1 : 0}"
   type              = "ingress"
   from_port         = "${var.http_port}"
   to_port           = "${var.http_port}"
