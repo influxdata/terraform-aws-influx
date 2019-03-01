b218b3b36d150f839d7a150a20bff3ac73ee1831
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2019-03-01 18:02:47 +0100

    update chronograf-server module to use single node ASG

--- modules/chronograf-server/variables.tf
+++ modules/chronograf-server/variables.tf
@@ -3,7 +3,7 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "server_name" {
+variable "cluster_name" {
   description = "The name of the Chronograf server. This variable is used to namespace all resources created by this module."
 }
 
@@ -17,6 +17,11 @@ variable "instance_type" {
 
 variable "vpc_id" {
   description = "The ID of the VPC in which to deploy the InfluxDB cluster"
+}
+
+variable "subnet_ids" {
+  description = "The subnet IDs into which the EC2 Instances should be deployed."
+  type        = "list"
 }
 
 variable "user_data" {
@@ -50,13 +55,106 @@ variable "allowed_ssh_security_group_ids_num" {
   default     = 0
 }
 
+variable "termination_policies" {
+  description = "A list of policies to decide how the instances in the auto scale group should be terminated. The allowed values are OldestInstance, NewestInstance, OldestLaunchConfiguration, ClosestToNextInstanceHour, Default."
+  default     = "Default"
+}
+
+variable "associate_public_ip_address" {
+  description = "If set to true, associate a public IP address with each EC2 Instance in the cluster."
+  default     = false
+}
+
+variable "spot_price" {
+  description = "The maximum hourly price to pay for EC2 Spot Instances."
+  default     = ""
+}
+
+variable "tenancy" {
+  description = "The tenancy of the instance. Must be one of: empty string, default, or dedicated. For EC2 Spot Instances only empty string or dedicated can be used."
+  default     = ""
+}
+
+variable "root_volume_ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized."
+  default     = false
+}
+
+variable "root_volume_type" {
+  description = "The type of volume. Must be one of: standard, gp2, or io1."
+  default     = "gp2"
+}
+
+variable "root_volume_size" {
+  description = "The size, in GB, of the root EBS volume."
+  default     = 50
+}
+
+variable "root_volume_delete_on_termination" {
+  description = "Whether the volume should be destroyed on instance termination."
+  default     = true
+}
+
+variable "root_volume_iops" {
+  description = "The amount of provisioned IOPS for the root volume. Only used if volume type is io1."
+  default     = 0
+}
+
+variable "ebs_block_devices" {
+  description = "A list of EBS volumes to attach to each EC2 Instance. Each item in the list should be an object with the keys 'device_name', 'volume_type', 'volume_size', 'iops', 'delete_on_termination', and 'encrypted', as defined here: https://www.terraform.io/docs/providers/aws/r/launch_configuration.html#block-devices. We recommend using one EBS Volume for each instance."
+  type        = "list"
+  default     = []
+
+  # Example:
+  #
+  # default = [
+  #   {
+  #     device_name = "/dev/xvdh"
+  #     volume_type = "gp2"
+  #     volume_size = 300
+  #     encrypted   = true
+  #   }
+  # ]
+}
+
+variable "wait_for_capacity_timeout" {
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. Setting this to '0' causes Terraform to skip all Capacity Waiting behavior."
+  default     = "10m"
+}
+
+variable "health_check_type" {
+  description = "Controls how health checking is done. Must be one of EC2 or ELB."
+  default     = "EC2"
+}
+
+variable "health_check_grace_period" {
+  description = "Time, in seconds, after instance comes into service before checking health."
+  default     = 600
+}
+
+variable "instance_profile_path" {
+  description = "Path in which to create the IAM instance profile."
+  default     = "/"
+}
+
 variable "ssh_port" {
   description = "The port used for SSH connections"
   default     = 22
 }
 
 variable "tags" {
-  description = "Tags to attach to the EC2 Instance."
-  type        = "map"
-  default     = {}
+  description = "List fo extra tag blocks added to the autoscaling group configuration. Each element in the list is a map containing keys 'key', 'value', and 'propagate_at_launch' mapped to the respective values."
+  type        = "list"
+  default     = []
+
+  # Example:
+  #
+  # default = [
+  #   {
+  #     key                 = "foo"
+  #     value               = "bar"
+  #     propagate_at_launch = true
+  #   }
+  # ]
 }
+
