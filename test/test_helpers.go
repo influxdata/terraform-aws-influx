3c3f45e89cc38f0094f90e21c3a20e3955addb03
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2019-03-15 00:13:05 +0100

    update kapacitor health check path in automated test

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -150,7 +150,7 @@ func validateChronograf(t *testing.T, endpoint string, port string) {
 func validateKapacitor(t *testing.T, endpoint string, port string) {
 	maxRetries := 30
 	sleepBetweenRetries := 4 * time.Second
-	url := fmt.Sprintf("http://%s:%s", endpoint, port)
+	url := fmt.Sprintf("http://%s:%s/kapacitor/v1/ping", endpoint, port)
 
 	logger.Log(t, "Checking URL: %s", url)
 
