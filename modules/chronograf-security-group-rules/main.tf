a2a95b8ca29d8858e7de11674a60a5e85c72d3c8
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2019-07-02 12:45:10 +0100

    Upgrade chronograf-security-group-rules module to tf12

--- modules/chronograf-security-group-rules/main.tf
+++ modules/chronograf-security-group-rules/main.tf
@@ -2,22 +2,31 @@
 # ATTACH SECURITY GROUP RULE TO ALLOW INCOMING CONNECTIONS ON CHRONGRAF'S LISTENING PORT
 # ---------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.12"
+}
+
 resource "aws_security_group_rule" "http_port_cidr_blocks" {
-  count             = "${length(var.http_port_cidr_blocks) >= 1 ? 1 : 0}"
+  count             = length(var.http_port_cidr_blocks) >= 1 ? 1 : 0
   type              = "ingress"
-  from_port         = "${var.http_port}"
-  to_port           = "${var.http_port}"
+  from_port         = var.http_port
+  to_port           = var.http_port
   protocol          = "tcp"
-  security_group_id = "${var.security_group_id}"
-  cidr_blocks       = ["${var.http_port_cidr_blocks}"]
+  security_group_id = var.security_group_id
+  cidr_blocks       = var.http_port_cidr_blocks
 }
 
 resource "aws_security_group_rule" "http_port_security_groups" {
-  count                    = "${var.http_port_security_groups_num}"
+  count                    = var.http_port_security_groups_num
   type                     = "ingress"
-  from_port                = "${var.http_port}"
-  to_port                  = "${var.http_port}"
+  from_port                = var.http_port
+  to_port                  = var.http_port
   protocol                 = "tcp"
-  security_group_id        = "${var.security_group_id}"
-  source_security_group_id = "${element(var.http_port_security_groups, count.index)}"
+  security_group_id        = var.security_group_id
+  source_security_group_id = element(var.http_port_security_groups, count.index)
 }
--- modules/chronograf-security-group-rules/variables.tf
+++ modules/chronograf-security-group-rules/variables.tf
@@ -5,10 +5,12 @@
 
 variable "security_group_id" {
   description = "The ID of the Security Group to which all the rules should be attached."
+  type        = string
 }
 
 variable "http_port" {
   description = "The HTTP port that Chronograf will listen on for connections"
+  type = number
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -18,17 +20,18 @@ variable "http_port" {
 
 variable "http_port_cidr_blocks" {
   description = "The list of IP address ranges in CIDR notation from which to allow connections to the http_port."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "http_port_security_groups" {
   description = "The list of Security Group IDs from which to allow connections to the http_port. If you update this variable, make sure to update var.http_port_security_groups_num too!"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "http_port_security_groups_num" {
   description = "The number of security group IDs in var.http_port_security_groups. We should be able to compute this automatically, but due to a Terraform limitation, if there are any dynamic resources in var.http_port_security_groups, then we won't be able to: https://github.com/hashicorp/terraform/pull/11482"
-  default     = 0
+  type        = number
+  default     = null
 }
