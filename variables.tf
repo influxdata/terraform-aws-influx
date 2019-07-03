746efd4ccd033a6b9437c51cd06e8254c349ee0c
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2019-07-03 16:11:55 +0100

    Upgrade main tf file to tf12

--- variables.tf
+++ variables.tf
@@ -26,35 +26,42 @@ variable "shared_secret" {
 
 variable "aws_region" {
   description = "The AWS region in which all resources will be created"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/influxdb-ami/influxdb.json."
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "influxdb_cluster_name" {
   description = "What to name the InfluxDB meta nodes cluster and all of its associated resources"
+  type        = string
   default     = "influxdb-cluster"
 }
 
 variable "ssh_key_name" {
   description = "The name of an EC2 Key Pair that can be used to SSH to the EC2 Instances in this cluster. Set to an empty string to not associate a Key Pair."
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "volume_device_name" {
   description = "The device name to use for the EBS Volume used for the meta, data, wal and hh directories on InfluxDB nodes."
+  type        = string
   default     = "/dev/xvdh"
 }
 
 variable "volume_mount_point" {
   description = "The mount point (folder path) to use for the EBS Volume used for the meta, data, wal and hh directories on InfluxDB data nodes."
+  type        = string
   default     = "/influxdb"
 }
 
 variable "volume_owner" {
   description = "The OS user who should be made the owner of mount points."
+  type        = string
   default     = "influxdb"
 }
