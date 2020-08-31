582b6f7af550a8a8da0058edc44c23287a17edd4
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-08-31 15:50:58 -0500

    Update to latest terratest and module-ci versions

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -167,7 +167,7 @@ func validateChronograf(t *testing.T, endpoint string, port string) {
 
 	logger.Log(t, "Checking URL: %s", url)
 
-	http_helper.HttpGetWithRetryWithCustomValidation(t, url, maxRetries, sleepBetweenRetries, func(status int, body string) bool {
+	http_helper.HttpGetWithRetryWithCustomValidation(t, url, nil, maxRetries, sleepBetweenRetries, func(status int, body string) bool {
 		return status == 200
 	})
 }
@@ -179,7 +179,7 @@ func validateKapacitor(t *testing.T, endpoint string, port string) {
 
 	logger.Log(t, "Checking URL: %s", url)
 
-	http_helper.HttpGetWithRetryWithCustomValidation(t, url, maxRetries, sleepBetweenRetries, func(status int, body string) bool {
+	http_helper.HttpGetWithRetryWithCustomValidation(t, url, nil, maxRetries, sleepBetweenRetries, func(status int, body string) bool {
 		return status == 204
 	})
 }
